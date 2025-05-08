32bbed06a1ecc036f964ddd7a07255dbf5335a4f
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-08 19:50:47 +0700

    commit

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -160,6 +160,37 @@ public:
                         << "Data dengan nomer mahasiswa " << nim << " berhasil dihapus " << endl;
 
             }
+            break;
+            case '3':
+            {
+                mhs.traverse();
+            }
+            break;
+
+            case '4':
+            {
+                if (mhs.listEmpty() == true)
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+            Node *previous, *current;
+            cout << endl
+                << "Masukan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+            cout << endl
+                << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                    << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa : " << current->noMhs << endl;
+                cout << "\n";
+            }
+              
+            }
+
 
 
 
