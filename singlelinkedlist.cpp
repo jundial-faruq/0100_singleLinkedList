797155b3e1ca9b7be588f317a39d5bae833f9b7f
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-08 19:26:17 +0700

    bool delNode dan void traverse

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -42,7 +42,7 @@ public:
     
         Node *previous = START;
         Node *current = START;
-    )
+    
         while ((current != NULL) && (nim >= current->noMhs))
         {
             if (nim == current->noMhs)
@@ -56,6 +56,8 @@ public:
         }
         nodeBaru->next = current;
         previous->next = nodeBaru;
+    }
+        
 
         bool listEmpty()
         {
@@ -69,9 +71,45 @@ public:
 
             while ((*current != NULL) && (nim != (*current)->noMhs))
             {
-                *privious = *current;
+                *previous = *current;
                 *current = (*current)->next;
+            }
+            return (*current != NULL);
+        }
+
+        
+
+        bool delNode(int nim)
+        {
+            Node *current, *previous;
+            if (!Search(nim, &previous, &current))
+                return false;
+            
+            if (current == START)
+                START = START->next;
+            else 
+                previous->next = current->next;
+
+                delete current;
+                return true;            
+        }  
+
+        void traverse()
+        {
+            if (listEmpty())
+            {
+                cout << "\nList Kosong\n";
+            }
+            else
+            {
+                cout << "\nData didalam list adalah :\n";
+                Node *currentNode = START;
+                while (currentNode != NULL)
+                {
+                    
+                }
+                
             }
             
         }
-};
+        
